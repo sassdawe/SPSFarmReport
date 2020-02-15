d5922cbe721937af5d6700553b3787441819e878
Sass David <skills@kolis.hu>
2020-02-15 22:17:20 +0100

    refactor(o16) PSUsing

--- The2016SPSFarmReport/Lib/function-PSUsing.ps1
+++ The2016SPSFarmReport/Lib/function-PSUsing.ps1
@@ -1,16 +1,23 @@
 function global:PSUsing {
-    param 
-    (        
-        [System.IDisposable] $inputObject = $(throw "The parameter -inputObject is required."),        
-        [ScriptBlock] $scriptBlock = $(throw "The parameter -scriptBlock is required.")    
-    )     
-    Try {        
-        &$scriptBlock    
-    } 
-    Finally {        
-        if ($inputObject -ne $null) {            
-            if ($inputObject.psbase -eq $null) {                $inputObject.Dispose()            } 
-            else {                $inputObject.psbase.Dispose()            }        
-        }    
+    [CmdletBinding()]
+    param (
+        [System.IDisposable]
+        $inputObject = $(throw "The parameter -inputObject is required.")
+        ,
+        [ScriptBlock]
+        $scriptBlock = $(throw "The parameter -scriptBlock is required.")
+    )
+    Try {
+        &$scriptBlock
+    }
+    Finally {
+        if ($null -ne $inputObject) {
+            if ($null -eq $inputObject.psbase) {
+                $inputObject.Dispose()
+            }
+            else {
+                $inputObject.psbase.Dispose()
+            }
+        }
     }
 }
