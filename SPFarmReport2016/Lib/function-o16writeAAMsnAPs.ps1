58a92f0182e1e02089b92ee782ba2a6f3d201d1b
David Sass <elethulyelenseg@hotmail.com>
2020-07-18 18:46:52 +0200

    fix(2016): o16writeAAMsnAPs null comparition

--- SPFarmReport2016/Lib/function-o16writeAAMsnAPs.ps1
+++ SPFarmReport2016/Lib/function-o16writeAAMsnAPs.ps1
@@ -1,19 +1,19 @@
 function o16writeAAMsnAPs() {
     try {
-        $AllZones = [Microsoft.SharePoint.Administration.SPUrlZone]::Default, 
-        [Microsoft.SharePoint.Administration.SPUrlZone]::Intranet, 
-        [Microsoft.SharePoint.Administration.SPUrlZone]::Internet, 
-        [Microsoft.SharePoint.Administration.SPUrlZone]::Custom, 
+        $AllZones = [Microsoft.SharePoint.Administration.SPUrlZone]::Default,
+        [Microsoft.SharePoint.Administration.SPUrlZone]::Intranet,
+        [Microsoft.SharePoint.Administration.SPUrlZone]::Internet,
+        [Microsoft.SharePoint.Administration.SPUrlZone]::Custom,
         [Microsoft.SharePoint.Administration.SPUrlZone]::Extranet
         $script:XMLWriter.WriteStartElement("Alternate_Access_Mappings")
         for ($wcount = 0; $wcount -lt $script:WebAppnum; $wcount++) {
             $XMLWriter.WriteStartElement("Web_Application")
             $XMLWriter.WriteAttributeString("Name", ($WebAppDetails[$wcount, 2]))
-			
-            for ($zones = 1; $zones -le 5; $zones++) {				
-                $XMLWriter.WriteStartElement(($AllZones[$zones - 1]))	
+
+            for ($zones = 1; $zones -le 5; $zones++) {
+                $XMLWriter.WriteStartElement(($AllZones[$zones - 1]))
                 $tempstr = $WebAppPublicAAM[$wcount, $zones]
-                if ($tempstr -ne $null) { 
+                if ($null -ne $tempstr) {
                     $tempstr = $tempstr.Trim()
                     if ($tempstr -ne "") {
                         $XMLWriter.WriteStartElement("PublicURL")
@@ -21,9 +21,9 @@ function o16writeAAMsnAPs() {
                         $XMLWriter.WriteEndElement()
                     }
                 }
-				
+
                 $tempstr = $WebAppInternalAAMURL[$wcount, $zones]
-                if ($tempstr -ne $null) {
+                if ($null -ne $tempstr) {
                     $tempstr = $tempstr.Trim()
                     if ($tempstr -ne "") {
                         $XMLWriter.WriteStartElement("InternalURL")
@@ -31,29 +31,29 @@ function o16writeAAMsnAPs() {
                         $XMLWriter.WriteEndElement()
                     }
                 }
-				
+
                 $tempstr = $WebAppAuthProviders[$wcount, $zones]
-                if ($tempstr -ne $null) {
+                if ($null -ne $tempstr) {
                     $tempstr = $tempstr -split ']'
-					
+
                     $XMLWriter.WriteStartElement("UseClaimsAuthentication")
                     $XMLWriter.WriteString(($tempstr[3].Trim()))
                     $XMLWriter.WriteEndElement()
-					
+
                     $XMLWriter.WriteStartElement("AuthenticationMode")
                     $XMLWriter.WriteString(($tempstr[0].Trim()))
-                    $XMLWriter.WriteEndElement()				
-					
+                    $XMLWriter.WriteEndElement()
+
                     $XMLWriter.WriteStartElement("AllowAnonymous")
                     $XMLWriter.WriteString(($tempstr[1].Trim()))
-                    $XMLWriter.WriteEndElement()								
+                    $XMLWriter.WriteEndElement()
 
                     $XMLWriter.WriteStartElement("Path")
                     $XMLWriter.WriteString(($tempstr[2].Trim()))
-                    $XMLWriter.WriteEndElement()				
+                    $XMLWriter.WriteEndElement()
                 }
                 $XMLWriter.WriteEndElement()
-            }		
+            }
             $XMLWriter.WriteEndElement()
         }
         $XMLWriter.WriteEndElement()
